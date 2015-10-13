a1c909825bb8810f1c362dff53597d2f9deea0e2
Akul Mehra <mehra.akul@gmail.com>
2015-10-14 00:36:39 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ISP-login
 A python script to automatically login my isp star broadband login page using Mechanize.
+
+# To run The program User must have following Dependencies:
+- Mechanize
+- shlex
+
+# Edit the Username and Password & Run The program using `python login.py`.
+
+# Autostart the program every 5min for best results.
+
+
---
+++ login.py
@@ -0,0 +1,26 @@
+import mechanize # To log in the page
+import shlex	 # To split a command
+import subprocess # To run a command
+
+#cmd will contain  ["ping","-c1","google.com"]     
+cmd = shlex.split("ping -c1 google.com")
+
+try:
+	""" This will Run the command and store its output in output."""
+	output = subprocess.check_output(cmd)
+	# print output
+except subprocess.CalledProcessError,e:
+	''' If the command failed, Open the Login page and Login. '''   	
+	# Will print the command failed with its exit status
+	
+	br = mechanize.Browser()
+	br.open("http://172.16.22.1/24online/webpages/clientloginfilter.jsp?loginstatus=null&logoutstatus=true&message=You+have+successfully+logged+off&liverequesttime=null&livemessage=null&url=null&isAccessDenied=false&fromlogout=null&sessionTimeout=-1.0&ipaddress=172.16.22.200&clientLoginReq=true")
+	br.select_form(nr=0)
+	br["username"]="admin"
+	br["password"]="1234"
+	response=(br.submit())
+	f=response.geturl()
+	
+	# print "Login Done"
+	
+
